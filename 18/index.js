af3cc9c78eefca624ec02d2bdb0eef2af0151237
Julian Møller <git@jumoel.com>
2019-12-19 17:44:09 +0100

    WIP

--- 18/index.js
+++ 18/index.js
@@ -4,14 +4,14 @@ const { map, sum, reduce, flatten, filter } = require("../util/array");
 const { clone } = require("../util/obj");
 const { ASSERT, logIdent } = require("../util/test");
 
-function clean(input) {
+function makeWorld(input) {
   return compose(trim, toLines, map(trim), map(split("")))(input);
 }
 
-function findInWorld(world, needle) {
+function findInWorld(world, fn) {
   for (let y = 0; y < world.length; y++) {
     for (let x = 0; x < world[y].length; x++) {
-      if (world[y][x] === needle) {
+      if (fn(world[y][x])) {
         return [x, y];
       }
     }
@@ -20,156 +20,164 @@ function findInWorld(world, needle) {
   return [-1, -1];
 }
 
-function findMatchesInWorld(world, fn) {
-  return compose(map(filter(x => fn(x))), flatten)(world);
+function findAllInWorld(world, fn) {
+  const res = [];
+
+  for (let y = 0; y < world.length; y++) {
+    for (let x = 0; x < world[y].length; x++) {
+      if (fn(world[y][x])) {
+        res.push([x, y, world[y][x]]);
+      }
+    }
+  }
+
+  return res;
 }
 
-function c(x, y) {
-  return `${x},${y}`;
+function k(x, y, l) {
+  return `${l.join(",")};${x},${y}`;
 }
 
-const ENTRY = "@";
-const WALL = "#";
 const PASSAGE = ".";
+const ENTRY = "@";
 const KEY_REGEX = /[a-z]/;
 const DOOR_REGEX = /[A-Z]/;
 
-function findAvailableKeysFromPos(world, pos) {
-  const visited = new Set();
-  const queue = [[...pos, 0]];
-  const result = [];
-  const yMax = world.length - 1;
-  const xMax = world[0].length - 1;
+function unlockAt(world, [x, y]) {
+  const newWorld = clone(world);
+  const found = newWorld[y][x];
+  const isKey = found.match(KEY_REGEX);
 
-  while (queue.length > 0) {
-    const [x, y, dist] = queue.shift();
-
-    if (x < 0 || y < 0 || x > xMax || y > yMax) {
-      continue;
-    }
-
-    if (visited.has(c(x, y))) {
-      continue;
-    }
-
-    visited.add(c(x, y));
-
-    const tile = world[y][x];
-
-    if (tile === WALL) {
-      continue;
-    }
-
-    if (tile.match(DOOR_REGEX) !== null) {
-      continue;
-    }
-
-    if (tile.match(KEY_REGEX)) {
-      result.push({ key: tile, x, y, dist });
-      continue;
-    }
-
-    queue.push(
-      [x + 1, y, dist + 1],
-      [x - 1, y, dist + 1],
-      [x, y + 1, dist + 1],
-      [x, y - 1, dist + 1],
+  if (isKey) {
+    newWorld[y][x] = PASSAGE;
+    const [doorX, doorY] = findInWorld(
+      newWorld,
+      x => x === found.toUpperCase(),
     );
+
+    if (doorX !== -1 && doorY !== -1) {
+      newWorld[doorY][doorX] = PASSAGE;
+    }
   }
 
-  return result;
+  return [newWorld, isKey && found];
 }
 
-function unlockDoor(world, key) {
-  const worldClone = clone(world);
-
-  const [doorX, doorY] = findInWorld(worldClone, key.key.toUpperCase());
-
-  worldClone[key.y][key.x] = PASSAGE;
-
-  if (doorX !== -1 && doorY !== -1) {
-    worldClone[doorY][doorX] = PASSAGE;
-  }
-
-  return worldClone;
-}
-
-function walkRecursive(world, startPos, keyToTake, keysLeft, dist, meme) {
-  console.log(...arguments);
-  if (keysLeft.length === 1) {
-    return dist + keyToTake.dist;
-  }
-
-  const hashKey =
-    String(startPos) +
-    ";" +
-    clone(keysLeft)
-      .sort()
-      .join("-");
-
-  if (meme.has(hashKey)) {
-    return meme.get(hashKey);
-  }
-
-  console.log(hashKey);
-
-  const nextWorld = unlockDoor(world, keyToTake);
-
-  const keys = findAvailableKeysFromPos(nextWorld, startPos);
-
-  const results = keys.map(nextKeyToTake =>
-    walkRecursive(
-      nextWorld,
-      [keyToTake.x, keyToTake.y],
-      nextKeyToTake,
-      keys
-        .map(k => k.key)
-        .filter(k => k !== keyToTake.key && nextKeyToTake.key !== k),
-      dist + keyToTake.dist,
-      meme,
-    ),
+function isPassable(w, [x, y]) {
+  return (
+    w[y][x] === ENTRY ||
+    w[y][x] === PASSAGE ||
+    Boolean(w[y][x].match(KEY_REGEX))
   );
+}
 
-  const result = Math.min(...results);
+function makePhoneBook(world, allKeys) {
+  const seen = {};
+  const maxX = world[0].length - 1;
+  const maxY = world.length - 1;
 
-  meme.set(hashKey, result);
+  for (let y = 0; y <= maxY; y++) {
+    for (let x = 0; x <= maxX; x++) {
+      const from = [x, y];
 
-  return result;
+      const queue = [[from, 0]];
+      const visited = new Set();
+
+      while (queue.length > 0) {
+        const [[cx, cy], dist] = queue.shift();
+
+        if (world[cy][cx] === "#") {
+          continue;
+        }
+
+        if (world[cy][cx].match(/[a-z]/)) {
+          seen[`${x},${y},${world[cy][cx]}`] = dist;
+        }
+
+        if (visited.has(`${cx},${cy}`)) {
+          continue;
+        }
+        visited.add(`${cx},${cy}`);
+
+        const next = [
+          [0, 1],
+          [0, -1],
+          [1, 0],
+          [-1, 0],
+        ]
+          .map(([dx, dy]) => {
+            return [cx + dx, cy + dy];
+          })
+          .filter(([nx, ny]) => nx >= 0 && nx <= maxX && ny >= 0 && ny <= maxY)
+          .filter(([nx, ny]) => world[ny][nx] !== "#")
+          .forEach(pos => queue.push([pos, dist + 1]));
+      }
+    }
+  }
+
+  return seen;
 }
 
 function part1(input) {
-  const world = clean(input);
-  const entryPos = findInWorld(world, ENTRY);
-  const keysLeft = findMatchesInWorld(world, x => x.match(KEY_REGEX) !== null);
+  const world = makeWorld(input);
+  const entry = findInWorld(world, x => x === ENTRY);
+  const allKeys = findAllInWorld(world, x => x.match(KEY_REGEX) !== null);
 
-  const entryKeys = findAvailableKeysFromPos(world, entryPos);
+  const phonebook = makePhoneBook(world, allKeys);
 
-  const meme = new Map();
+  let queue = [[clone(world), entry, [], 0]];
 
-  return Math.min(
-    ...entryKeys.map(keyToTake =>
-      walkRecursive(
-        world,
-        entryPos,
-        keyToTake,
-        keysLeft.filter(k => k !== keyToTake.key),
-        0,
-        meme,
-      ),
-    ),
-  );
+  let min = Infinity;
+  const memo = new Set();
+
+  while (queue.length > 0) {
+    const [w, [x, y], seen, dist] = queue.shift();
+    const hk = k(x, y, seen);
+
+    if (memo.has(hk)) {
+      continue;
+    }
+    memo.add(hk);
+
+    if (seen.length === allKeys.length) {
+      if (dist - 1 < min) {
+        min = dist - 1;
+      }
+
+      continue;
+    }
+
+    const [w2, unlockedKey] = unlockAt(w, [x, y]);
+
+    // don't do this, but find the visible keys from
+    // the current position and use the phonebook to find
+    // the distances to them
+
+    const nextSteps = [
+      [0, 1],
+      [-1, 0],
+      [0, -1],
+      [1, 0],
+    ]
+      .filter(([dx, dy]) => isPassable(w2, [x + dx, y + dy]))
+      .map(([dx, dy]) => {
+        return [
+          clone(w2),
+          [x + dx, y + dy],
+          seen.concat([unlockedKey].filter(Boolean)),
+          dist + 1,
+        ];
+      });
+
+    queue = queue.concat(nextSteps);
+  }
+
+  return min;
 }
 
 function part2(input) {
   return input;
 }
-
-module.exports = {
-  part1,
-  findAvailableKeysFromPos,
-  clean,
-  findInWorld,
-};
 
 if (require.main === module) {
   const { readInput } = require("../util/readInput");
