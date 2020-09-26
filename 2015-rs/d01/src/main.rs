66b94685bb7aff7b18ade397592d1e86a65ea8b0
Julian Møller <git@jumoel.com>
2020-09-26 22:28:18 +0200

    Add 2015 day 1, 2 and 3 in Rust

---
+++ 2015-rs/d01/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+
+fn main() {
+    let map = fs::read_to_string("d01/input.txt")
+        .expect("Something went wrong reading the file");
+
+    let part1 = map.chars().fold(0, |acc, c| match c {
+        '(' => acc + 1,
+        ')' => acc - 1,
+        _ => acc
+    });
+
+    println!("Part 1: {}", part1);
+
+    let part2_partial: Result<i32, usize> = map.chars().enumerate().try_fold(0, |acc, (i, c)| {
+        if acc == -1 {
+            return Err(i)
+        }
+
+        let val = match c {
+            '(' => (acc + 1),
+            ')' => (acc - 1),
+            _ => (acc),
+        };
+
+        Ok(val)
+    });
+
+    let part2 = match part2_partial {
+        Ok(_) => map.chars().count(),
+        Err(c) => c,
+    };
+
+    println!("Part 2: {}", part2);
+
+}
--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 These are my solutions for the Advent of Code problems.
 
-* [2015: ReasonML](/2015)
-* 2016: Nothing yet
-* 2017: Nothing yet
-* 2018: Nothing yet
-* [2019: JavaScript](/2019)
-* 2020: Will probably be in Rust
+- [2015: Rust](/2015-rs) / [2015: ReasonML](/2015-re) \*
+- 2016: Nothing yet
+- 2017: Nothing yet
+- 2018: Nothing yet
+- [2019: JavaScript](/2019)
+- 2020: Looking forward to it
+
+\*: I started solving the 2015 problems with native ReasonML in early 2020. When I got back to it in the fall of 2020, I couldn't remember how `esy` or `dune` worked, so I started over in Rust. 🤷‍♂️
