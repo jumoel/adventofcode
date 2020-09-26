808c94a42d3b215b98bedc630da996077c1700cd
Julian Møller <git@jumoel.com>
2020-09-26 22:28:18 +0200

    Add 2015 day 1, 2 and 3 in Rust

---
+++ 2015-rs/d02/src/main.rs
@@ -0,0 +1,48 @@
+use std::cmp::{max, min};
+use std::fs;
+
+fn main() {
+	let input = fs::read_to_string("d02/input.txt")
+		.expect("Something went wrong reading the file");
+
+	let input_clean = input
+		.lines()
+		.map(|l| {
+			let parts = l
+				.split("x")
+				.map(|p| {
+					p.parse::<i32>().expect("Invalid input types")
+				})
+				.collect::<Vec<i32>>();
+
+			if parts.len() != 3 {
+				return None;
+			}
+
+			Some((parts[0], parts[1], parts[2]))
+		})
+		.map(|v| v.expect("Invalid input length"))
+		.collect::<Vec<(i32, i32, i32)>>();
+
+	let part1: i32 = input_clean
+		.iter()
+		.map(|(w, h, l)| {
+			let a = w * h;
+			let b = w * l;
+			let c = h * l;
+
+			2 * a + 2 * b + 2 * c + min(a, min(b, c))
+		})
+		.sum();
+
+	println!("Part 1: {}", part1);
+
+	let part2: i32 = input_clean
+		.iter()
+		.map(|(w, d, h)| {
+			w * d * h + 2 * w + 2 * h + 2 * d - 2 * max(w, max(d, h))
+		})
+		.sum();
+
+	println!("Part 2: {}", part2);
+}
---
+++ 2015-rs/d03/src/main.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+use std::fs;
+use std::str::Chars;
+
+fn santa(cs: Chars, visited: &mut HashMap<(i32, i32), i32>) -> () {
+	cs.fold((0, 0), |(x, y), dir| {
+		let count = visited.entry((x, y)).or_insert(0);
+
+		*count += 1;
+
+		match dir {
+			'^' => (x, y + 1),
+			'v' => (x, y - 1),
+			'<' => (x - 1, y),
+			'>' => (x + 1, y),
+			_ => (x, y),
+		}
+	});
+}
+
+fn main() {
+	let input = fs::read_to_string("d03/input.txt")
+		.expect("Something went wrong reading the file");
+
+	let mut visited1 = HashMap::new();
+
+	santa(input.chars(), &mut visited1);
+
+	println!("Part 1: {}", visited1.len());
+
+	// let mut visited2 = HashMap::new();
+
+	// santa(input.chars().step_by(2), &mut visited2);
+	// santa(input.chars().skip(1).step_by(2), &mut visited2);
+
+	// input.chars().unzip()
+
+	println!("Part 2: {}", 0);
+}
