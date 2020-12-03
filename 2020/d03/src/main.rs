0121b94b7139c64ec77ab3d470d6cec3537a4a72
Julian Møller <git@jumoel.com>
2020-12-03 12:28:50 +0100

    2020/3/2

--- 2020/d03/src/main.rs
+++ 2020/d03/src/main.rs
@@ -5,17 +5,15 @@ type Result<T> =
 
 const TREE: char = '#';
 
-fn main() -> Result<()> {
-	let input = fs::read_to_string("d03/input.txt")?;
-
+fn slope(map: &str, dx: usize, dy: usize) -> Result<i32> {
 	let mut trees = 0;
 
 	let mut offset: usize = 0;
 
 	let width =
-		input.lines().nth(0).ok_or("invalid data")?.chars().count();
+		map.lines().nth(0).ok_or("invalid data")?.chars().count();
 
-	for line in input.lines() {
+	for line in map.lines().step_by(dy) {
 		let coord =
 			line.chars().nth(offset).ok_or("invalid offset")?;
 
@@ -23,11 +21,33 @@ fn main() -> Result<()> {
 			trees += 1;
 		}
 
-		offset = (offset + 3) % width;
+		offset = (offset + dx) % width;
 	}
 
-	println!("{:?}", trees);
-	// println!("{:?}", part2);
+	Ok(trees)
+}
+
+fn part1(map: &str) -> Result<i32> {
+	slope(&map, 3, 1)
+}
+
+fn part2(map: &str) -> Result<i32> {
+	let parts = vec![
+		slope(&map, 1, 1)?,
+		slope(&map, 3, 1)?,
+		slope(&map, 5, 1)?,
+		slope(&map, 7, 1)?,
+		slope(&map, 1, 2)?,
+	];
+
+	Ok(parts.iter().fold(1, |acc, v| acc * v))
+}
+
+fn main() -> Result<()> {
+	let input = fs::read_to_string("d03/input.txt")?;
+
+	println!("{:?}", part1(&input)?);
+	println!("{:?}", part2(&input)?);
 
 	Ok(())
 }
