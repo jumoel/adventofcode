5be870f41ce8444af03290beace5cea8a7db85a4
Julian Møller <git@jumoel.com>
2020-12-06 20:20:00 +0100

    2020/5/2

--- 2020/d05/src/main.rs
+++ 2020/d05/src/main.rs
@@ -32,19 +32,41 @@ fn process_pass(pass: &str) -> Result<i32> {
 	Ok(row * 8 + seat)
 }
 
+fn find_ids(
+	input: &str,
+) -> impl std::iter::Iterator<Item = i32> + '_ {
+	input.lines().map(|line| process_pass(line).unwrap_or(0))
+}
+
+fn part1(input: &str) -> i32 {
+	find_ids(input).max().unwrap_or(0)
+}
+
+fn part2(input: &str) -> i32 {
+	let mut ids: Vec<i32> = find_ids(input).collect();
+
+	ids.sort();
+
+	let pairs: Vec<(usize, &i32)> = ids.iter().enumerate().collect();
+
+	*pairs
+		.iter()
+		.skip(1)
+		.find(|(pos, e)| {
+			let prev = pairs.get(*pos - 1).unwrap();
+			let prev_val = **e - 1;
+
+			*prev.1 != prev_val
+		})
+		.unwrap_or(&(0, &0))
+		.1 - 1
+}
+
 fn main() -> Result<()> {
 	let input = fs::read_to_string("d05/input.txt")?;
 
-	let part1 = input
-		.lines()
-		.map(|line| process_pass(line).unwrap_or(0))
-		.max()
-		.unwrap_or(0);
-
-	// let part2 = part2(&passports, &reqs)?;
-
-	println!("{:?}", part1);
-	// println!("{:?}", part2);
+	println!("{:?}", part1(&input));
+	println!("{:?}", part2(&input));
 
 	Ok(())
 }
