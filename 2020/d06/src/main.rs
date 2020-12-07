ed7a7cdf33b4601896f6c3823fbce65d553383d1
Julian Møller <git@jumoel.com>
2020-12-07 07:55:52 +0100

    2020/6/2

--- 2020/d06/src/main.rs
+++ 2020/d06/src/main.rs
@@ -3,10 +3,8 @@ use std::{collections::HashSet, fs};
 type Result<T> =
 	::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
-fn main() -> Result<()> {
-	let input = fs::read_to_string("d06/input.txt")?;
-
-	let part1: usize = input
+fn part1(input: &str) -> usize {
+	input
 		.split("\n\n")
 		.map(|group| {
 			group
@@ -15,9 +13,37 @@ fn main() -> Result<()> {
 				.collect::<HashSet<char>>()
 				.len()
 		})
-		.sum();
+		.sum()
+}
 
-	println!("{:?}", part1);
+fn part2(input: &str) -> usize {
+	input
+		.split("\n\n")
+		.map(|group| {
+			let mut individual_answers =
+				group.split_ascii_whitespace().map(|answers| {
+					answers.chars().collect::<HashSet<char>>()
+				});
+
+			let first =
+				individual_answers.nth(0).unwrap_or(HashSet::new());
+
+			individual_answers
+				.fold(first, |mut acc, elem| {
+					acc.retain(|p| elem.contains(p));
+
+					acc
+				})
+				.len()
+		})
+		.sum()
+}
+
+fn main() -> Result<()> {
+	let input = fs::read_to_string("d06/input.txt")?;
+
+	println!("{:?}", part1(&input));
+	println!("{:?}", part2(&input));
 
 	Ok(())
 }
