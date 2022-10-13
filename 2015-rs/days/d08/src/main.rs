c6bf6bf92e97303f50721167582e4172b1f9f33c
Julian Møller Ellehauge <git@jumoel.com>
2022-10-13 22:37:46 +0200

    2015-08, part 2, with serde

--- 2015-rs/days/d08/src/main.rs
+++ 2015-rs/days/d08/src/main.rs
@@ -2,17 +2,14 @@ use std::fs;
 
 use regex::Regex;
 
-fn main() {
-	let input = fs::read_to_string("days/d08/input.txt")
-		.expect("Something went wrong reading the file");
-
+fn part1(lines: &Vec<&str>) -> usize {
 	let res = [r#"(\\\\|\\")"#, r"\\x[a-z0-9]{2}"]
 		.map(|pattern| Regex::new(pattern).unwrap())
 		.to_vec();
 
-	let input: usize = input
-		.lines()
-		.filter_map(|line| {
+	lines
+		.into_iter()
+		.map(|line| {
 			let line_truncated: String =
 				line.chars().skip(1).take(line.len() - 2).collect();
 
@@ -21,9 +18,26 @@ fn main() {
 					re.replace_all(&acc, "x".to_string()).to_string()
 				});
 
-			Some(line.len() - line_truncated.len())
+			line.len() - line_truncated.len()
 		})
-		.sum();
+		.sum()
+}
 
-	println!("Part 1: {}", input);
+fn part2(lines: &Vec<&str>) -> usize {
+	lines
+		.into_iter()
+		.map(|line| {
+			serde_json::to_string(line).unwrap().len() - line.len()
+		})
+		.sum()
+}
+
+fn main() {
+	let input = fs::read_to_string("days/d08/input.txt")
+		.expect("Something went wrong reading the file");
+
+	let input: Vec<&str> = input.lines().collect();
+
+	println!("Part 1: {}", part1(&input));
+	println!("Part 2: {}", part2(&input));
 }
