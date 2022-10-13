3fee7d9c83897449ee8838bcef2f01d11f0357c0
Julian Møller Ellehauge <git@jumoel.com>
2022-10-13 11:00:11 +0200

    Apply 'cargo fmt'

--- 2015-rs/d04/src/main.rs
+++ 2015-rs/d04/src/main.rs
@@ -1,26 +1,30 @@
 use std::fs;
 
 fn hash(s: &str, suffix: i32) -> String {
-    format!("{:x}", md5::compute(format!("{}{}", s, suffix.to_string())))
+	format!(
+		"{:x}",
+		md5::compute(format!("{}{}", s, suffix.to_string()))
+	)
 }
 
 fn main() {
-    let input = fs::read_to_string("d04/input.txt").expect("Something went wrong reading the file");
+	let input = fs::read_to_string("d04/input.txt")
+		.expect("Something went wrong reading the file");
 
-    let mut suffix: i32 = 0;
-    let mut result = hash(&input, suffix);
+	let mut suffix: i32 = 0;
+	let mut result = hash(&input, suffix);
 
-    while !result.starts_with("00000") {
-        suffix += 1;
-        result = hash(&input, suffix);
-    }
+	while !result.starts_with("00000") {
+		suffix += 1;
+		result = hash(&input, suffix);
+	}
 
-    println!("Part 1: {}", suffix);
+	println!("Part 1: {}", suffix);
 
-    while !result.starts_with("000000") {
-        suffix += 1;
-        result = hash(&input, suffix);
-    }
+	while !result.starts_with("000000") {
+		suffix += 1;
+		result = hash(&input, suffix);
+	}
 
-    println!("Part 2: {}", suffix);
+	println!("Part 2: {}", suffix);
 }
