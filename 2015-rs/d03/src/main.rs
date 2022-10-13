54e81ade7df6ffe50ed259c78ef8059e00540a10
Julian Møller Ellehauge <git@jumoel.com>
2022-10-13 11:00:11 +0200

    Apply 'cargo fmt'

--- 2015-rs/d03/src/main.rs
+++ 2015-rs/d03/src/main.rs
@@ -1,35 +1,39 @@
 use std::collections::HashMap;
 use std::fs;
 
-fn santa(cs: &mut dyn Iterator<Item = char>, visited: &mut HashMap<(i32, i32), i32>) -> () {
-    cs.fold((0, 0), |(x, y), dir| {
-        let count = visited.entry((x, y)).or_insert(0);
+fn santa(
+	cs: &mut dyn Iterator<Item = char>,
+	visited: &mut HashMap<(i32, i32), i32>,
+) -> () {
+	cs.fold((0, 0), |(x, y), dir| {
+		let count = visited.entry((x, y)).or_insert(0);
 
-        *count += 1;
+		*count += 1;
 
-        match dir {
-            '^' => (x, y + 1),
-            'v' => (x, y - 1),
-            '<' => (x - 1, y),
-            '>' => (x + 1, y),
-            _ => (x, y),
-        }
-    });
+		match dir {
+			'^' => (x, y + 1),
+			'v' => (x, y - 1),
+			'<' => (x - 1, y),
+			'>' => (x + 1, y),
+			_ => (x, y),
+		}
+	});
 }
 
 fn main() {
-    let input = fs::read_to_string("d03/input.txt").expect("Something went wrong reading the file");
+	let input = fs::read_to_string("d03/input.txt")
+		.expect("Something went wrong reading the file");
 
-    let mut visited1 = HashMap::new();
+	let mut visited1 = HashMap::new();
 
-    santa(&mut input.chars(), &mut visited1);
+	santa(&mut input.chars(), &mut visited1);
 
-    println!("Part 1: {}", visited1.len());
+	println!("Part 1: {}", visited1.len());
 
-    let mut visited2 = HashMap::new();
+	let mut visited2 = HashMap::new();
 
-    santa(&mut input.chars().step_by(2), &mut visited2);
-    santa(&mut input.chars().skip(1).step_by(2), &mut visited2);
+	santa(&mut input.chars().step_by(2), &mut visited2);
+	santa(&mut input.chars().skip(1).step_by(2), &mut visited2);
 
-    println!("Part 2: {}", visited2.len());
+	println!("Part 2: {}", visited2.len());
 }
