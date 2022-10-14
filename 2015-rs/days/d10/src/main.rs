0992864113c23f3432b8a6c9b3a03fcea4768fa9
Julian Møller Ellehauge <git@jumoel.com>
2022-10-14 14:47:04 +0200

    2015-10

--- 2015-rs/days/d10/src/main.rs
+++ 2015-rs/days/d10/src/main.rs
@@ -3,23 +3,35 @@ use std::fs;
 type Result<T> =
 	::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
+fn lookandsay(input: String) -> String {
+	let mut iter = input.trim().chars();
+
+	let mut result = String::from("");
+	let mut same = 0;
+
+	while let Some(needle) = iter.nth(same) {
+		same = iter.clone().take_while(|c| c.eq(&needle)).count();
+		result.push_str(&format!("{}{}", same + 1, needle));
+	}
+
+	result
+}
+
+fn repeat(input: &String, count: usize) -> usize {
+	let mut result = String::from(input);
+
+	for _ in 0..count {
+		result = lookandsay(result)
+	}
+
+	result.len()
+}
+
 fn main() -> Result<()> {
 	let input = fs::read_to_string("days/d10/input.txt")?;
 
-	println!("input: {}", input);
-
-	let mut iter = input.trim().chars();
-
-	let mut lookandsay = String::from("");
-	let mut nth = 0;
-
-	while let Some(needle) = iter.nth(nth) {
-		let same = iter.clone().take_while(|c| c.eq(&needle)).count();
-		lookandsay.push_str(&format!("{}{}", same + 1, needle));
-		nth = same;
-	}
-
-	println!("{}", lookandsay);
+	println!("Part 1: {}", repeat(&input, 40));
+	println!("Part 2: {}", repeat(&input, 50));
 
 	Ok(())
 }
