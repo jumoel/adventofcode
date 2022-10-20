c12f0767b2e08ca4892848c25f4b43bf21686985
Julian Møller Ellehauge <git@jumoel.com>
2022-10-20 23:15:15 +0200

    2015, day 16, part 2

--- 2015-rs/days/d16/src/main.rs
+++ 2015-rs/days/d16/src/main.rs
@@ -4,26 +4,87 @@ use regex::Regex;
 
 type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
+fn finder(
+	clues: &HashMap<&str, i32>,
+	sues: &Vec<(i32, HashMap<&str, i32>)>,
+	func: Box<dyn Fn(&str, &i32, &i32) -> Option<()>>,
+) -> Result<i32> {
+	let sue = sues
+		.iter()
+		.map(|(num, mem)| {
+			let matching_clues = clues
+				.iter()
+				.filter_map(|(sue_clue, count)| {
+					let clue_val = mem.get(sue_clue)?;
+
+					func(*sue_clue, clue_val, count)
+				})
+				.count();
+
+			(num, matching_clues)
+		})
+		.max_by_key(|s| s.1)
+		.ok_or("Part 1 failed")?;
+
+	Ok(*sue.0)
+}
+
+fn part1(clues: &HashMap<&str, i32>, sues: &Vec<(i32, HashMap<&str, i32>)>) -> Result<i32> {
+	finder(
+		clues,
+		sues,
+		Box::new(
+			|_: &str, clue_val: &i32, count: &i32| {
+				if clue_val == count {
+					Some(())
+				} else {
+					None
+				}
+			},
+		),
+	)
+}
+
+fn part2(clues: &HashMap<&str, i32>, sues: &Vec<(i32, HashMap<&str, i32>)>) -> Result<i32> {
+	finder(
+		clues,
+		sues,
+		Box::new(
+			|sue_clue: &str, clue_val: &i32, count: &i32| match sue_clue {
+				"cats" if clue_val > count => Some(()),
+				"trees" if clue_val > count => Some(()),
+				"pomeranians" if clue_val < count => Some(()),
+				"goldfish" if clue_val < count => Some(()),
+				_ if clue_val == count => Some(()),
+				_ => None,
+			},
+		),
+	)
+}
+
 fn main() -> Result<()> {
 	let input = fs::read_to_string("days/d16/input.txt")?;
 
 	let sue_re = Regex::new(r"Sue (\d+):")?;
 	let clue_re = Regex::new(r" ((\w+): (\d+))")?;
 
-	let sues = input.lines().filter_map(|line| {
-		let sue = i32::from_str_radix(sue_re.captures(line)?.get(1)?.as_str(), 10).ok()?;
-		let clues = clue_re
-			.captures_iter(line)
-			.filter_map(|cap| {
-				let name = cap.get(2)?.as_str();
-				let val = i32::from_str_radix(cap.get(3)?.as_str(), 10).ok()?;
+	let sues = input
+		.lines()
+		.filter_map(|line| {
+			let sue = i32::from_str_radix(sue_re.captures(line)?.get(1)?.as_str(), 10).ok()?;
+			let clues = clue_re
+				.captures_iter(line)
+				.filter_map(|cap| {
+					let name = cap.get(2)?.as_str();
+					let val = i32::from_str_radix(cap.get(3)?.as_str(), 10).ok()?;
 
-				Some((name, val))
-			})
-			.collect::<HashMap<_, _>>();
+					Some((name, val))
+				})
+				.collect::<HashMap<_, _>>();
 
-		Some((sue, clues))
-	});
+			Some((sue, clues))
+		})
+		.collect::<Vec<_>>();
 
 	let clues = HashMap::from([
 		("children", 3),
@@ -38,29 +99,8 @@ fn main() -> Result<()> {
 		("perfumes", 1),
 	]);
 
-	let part1 = sues
-		.map(|(num, mem)| {
-			let matching_clues =
-				clues
-					.iter()
-					.filter_map(|(sue_clue, count)| {
-						mem.get(sue_clue).and_then(|clue_val| {
-							if clue_val == count {
-								Some(count)
-							} else {
-								None
-							}
-						})
-					})
-					.count();
-
-			(num, matching_clues)
-		})
-		.max_by_key(|s| s.1)
-		.ok_or("Part 1 failed")?
-		.0;
-
-	println!("Part 1: {}", part1);
+	println!("Part 1: {}", part1(&clues, &sues)?);
+	println!("Part 2: {}", part2(&clues, &sues)?);
 
 	Ok(())
 }
