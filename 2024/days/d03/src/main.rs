1b489cf4b4a17cc75576e2e98fb79456553a25f4
Julian Møller Ellehauge <git@jumoel.com>
2024-12-12 01:25:30 +0100

    2024, d3, pt2

--- 2024/days/d03/src/main.rs
+++ 2024/days/d03/src/main.rs
@@ -13,46 +13,69 @@ use nom::{
 
 type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
-#[derive(Debug, Copy, Clone)]
-struct Mul {
-    a: i32,
-    b: i32,
+#[derive(Debug)]
+enum Op {
+    Garbage,
+    Do,
+    Dont,
+    Mul(i32, i32),
 }
 
-fn mul_or_skip(input: &str) -> IResult<&str, Vec<Option<Mul>>> {
-    many0(alt((parse_mul, skip)))(input)
+fn parser(input: &str) -> IResult<&str, Vec<Op>> {
+    many0(alt((parse_mul, parse_do, parse_dont, skip)))(input)
 }
 
-fn skip(s: &str) -> IResult<&str, Option<Mul>> {
-    map(take(1usize), |_| None)(s)
+fn skip(s: &str) -> IResult<&str, Op> {
+    map(take(1usize), |_| Op::Garbage)(s)
 }
 
 fn number(s: &str) -> IResult<&str, i32> {
     map_res(digit1, FromStr::from_str)(s)
 }
 
-fn parse_mul(s: &str) -> IResult<&str, Option<Mul>> {
+fn parse_do(s: &str) -> IResult<&str, Op> {
+    map(tag("do()"), |_| Op::Do)(s)
+}
+
+fn parse_dont(s: &str) -> IResult<&str, Op> {
+    map(tag("don't()"), |_| Op::Dont)(s)
+}
+
+fn parse_mul(s: &str) -> IResult<&str, Op> {
     map(
         delimited(
             tag("mul("),
             separated_pair(number, tag(","), number),
             tag(")"),
         ),
-        |(a, b)| Some(Mul { a, b }),
+        |(a, b)| Op::Mul(a, b),
     )(s)
 }
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("inputs/d03.txt")?;
 
-    let res = mul_or_skip(&input)
-        .unwrap()
-        .1
+    let instructions = parser(&input).unwrap().1;
+
+    let part1 = instructions
         .iter()
-        .filter_map(|x| *x)
-        .map(|x| x.a * x.b)
+        .filter_map(|x| match x {
+            Op::Mul(a, b) => Some(a * b),
+            _ => None,
+        })
         .sum::<i32>();
-    println!("{:?}", res);
+    println!("Part 1: {:?}", part1);
+
+    let part2 = instructions
+        .iter()
+        .fold((0, true), |(sum, enabled), op| match op {
+            Op::Do => (sum, true),
+            Op::Dont => (sum, false),
+            Op::Mul(a, b) if enabled => (sum + a * b, enabled),
+            _ => (sum, enabled),
+        })
+        .0;
+    println!("Part 2: {:?}", part2);
 
     Ok(())
 }
