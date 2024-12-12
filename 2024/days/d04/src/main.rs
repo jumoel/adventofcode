9f338a04cd1ea2d1ff81db73a63f4447eeb83973
Julian Møller Ellehauge <git@jumoel.com>
2024-12-12 10:56:57 +0100

    2024-04-2

--- 2024/days/d04/src/main.rs
+++ 2024/days/d04/src/main.rs
@@ -14,7 +14,7 @@ enum Dir {
     SW,
 }
 
-const TARGET: [char; 4] = ['X', 'M', 'A', 'S'];
+const XMAS: [char; 4] = ['X', 'M', 'A', 'S'];
 const DIRS: [Dir; 8] = [
     Dir::N,
     Dir::S,
@@ -26,7 +26,12 @@ const DIRS: [Dir; 8] = [
     Dir::SW,
 ];
 
-fn check_word(crossword: &Vec<Vec<char>>, x: usize, y: usize, dir: &Dir) -> bool {
+fn get(x: i32, y: i32, crossword: &Vec<Vec<char>>) -> Result<&char> {
+    let row = crossword.get(y as usize).context("Out of bounds")?;
+    row.get(x as usize).context("Out of bounds")
+}
+
+fn check_xmas(crossword: &Vec<Vec<char>>, x: usize, y: usize, dir: &Dir) -> bool {
     let dx: i32 = match dir {
         Dir::N | Dir::S => 0,
         Dir::E | Dir::SE | Dir::NE => 1,
@@ -39,8 +44,7 @@ fn check_word(crossword: &Vec<Vec<char>>, x: usize, y: usize, dir: &Dir) -> bool
         Dir::N | Dir::NE | Dir::NW => -1,
     };
 
-    TARGET
-        .iter()
+    XMAS.iter()
         .enumerate()
         .map(|(i, c)| -> Result<bool> {
             let ni: i32 = i.try_into()?;
@@ -51,8 +55,7 @@ fn check_word(crossword: &Vec<Vec<char>>, x: usize, y: usize, dir: &Dir) -> bool
             let mut ny: i32 = y.try_into()?;
             ny += ni * dy;
 
-            let row = crossword.get(ny as usize).context("Out of bounds")?;
-            let cell = row.get(nx as usize).context("Out of bounds")?;
+            let cell = get(nx, ny, crossword)?;
 
             Ok(*cell == *c)
         })
@@ -60,6 +63,63 @@ fn check_word(crossword: &Vec<Vec<char>>, x: usize, y: usize, dir: &Dir) -> bool
             Ok(true) => true,
             _ => false,
         })
+}
+
+fn part1(crossword: &Vec<Vec<char>>) -> usize {
+    crossword.iter().enumerate().fold(0, |mut sum, (y, row)| {
+        sum += row.iter().enumerate().fold(0, |mut sum, (x, c)| {
+            if *c != XMAS[0] {
+                return sum;
+            }
+
+            sum += DIRS
+                .iter()
+                .filter(|dir| check_xmas(&crossword, x, y, dir))
+                .count();
+
+            sum
+        });
+
+        sum
+    })
+}
+
+type Point = (i32, i32);
+type Line = (Point, Point);
+const XDIRS: [Line; 2] = [((-1, -1), (1, 1)), ((-1, 1), (1, -1))];
+
+fn part2(cw: &Vec<Vec<char>>) -> usize {
+    cw.iter().enumerate().fold(0, |mut sum, (y, row)| {
+        sum += row.iter().enumerate().fold(0, |mut sum, (x, c)| {
+            if *c != 'A' {
+                return sum;
+            }
+
+            let is_cross = XDIRS
+                .iter()
+                .map(|((x1, y1), (x2, y2))| -> Result<bool> {
+                    let nx: i32 = x.try_into()?;
+                    let ny: i32 = y.try_into()?;
+
+                    let (nx1, ny1, nx2, ny2) = (nx + x1, ny + y1, nx + x2, ny + y2);
+                    let (c1, c2) = (get(nx1, ny1, cw), get(nx2, ny2, cw));
+
+                    match (c1, c2) {
+                        (Ok('S'), Ok('M')) | (Ok('M'), Ok('S')) => Ok(true),
+                        _ => Ok(false),
+                    }
+                })
+                .all(|x| matches!(x, Ok(true)));
+
+            if is_cross {
+                sum += 1;
+            }
+
+            sum
+        });
+
+        sum
+    })
 }
 
 fn main() -> Result<()> {
@@ -70,24 +130,8 @@ fn main() -> Result<()> {
         .map(|l| l.chars().collect())
         .collect::<Vec<Vec<char>>>();
 
-    let part1 = crossword.iter().enumerate().fold(0, |mut sum, (y, row)| {
-        sum += row.iter().enumerate().fold(0, |mut sum, (x, c)| {
-            if *c != TARGET[0] {
-                return sum;
-            }
-
-            sum += DIRS
-                .iter()
-                .filter(|dir| check_word(&crossword, x, y, dir))
-                .count();
-
-            sum
-        });
-
-        sum
-    });
-
-    println!("Part 1: {:?}", part1);
+    println!("Part 1: {:?}", part1(&crossword));
+    println!("Part 2: {:?}", part2(&crossword));
 
     Ok(())
 }
