c37b714cb0ef218a88e4788f4a534396b50b8bd5
Julian Møller Ellehauge <git@jumoel.com>
2025-05-26 13:25:04 +0200

    update 2019 to read from new input

--- 2019/util/readInput.js
+++ 2019/util/readInput.js
@@ -2,7 +2,13 @@ const fs = require("fs");
 const path = require("path");
 
 function readInput(dir, filename = "input.txt", trim = true) {
-  const str = fs.readFileSync(path.resolve(dir, "./" + filename)).toString();
+  const parts = dir.split("/")
+  const day = Number.parseInt(parts.pop())
+  const year = Number.parseInt(parts.pop())
+
+  const root = path.resolve(__dirname, '../../')
+
+  const str = fs.readFileSync(path.resolve(root, `input/${year}/${day}/${filename}`)).toString();
 
   return trim ? str.trim() : str;
 }
